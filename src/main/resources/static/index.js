895f64a9e59a1785c26500cad1cb8484d90bb27b
mka-Mikhail <mixailrojnow@yandex.ru>
2022-12-04 14:41:54 +0300

    filtering products

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,9 +1,17 @@
 angular.module('app', []).controller('indexController', function ($scope, $http) {
     const contextPath = 'http://localhost:8080/app';
 
-    $scope.loadProducts = function () {
-        $http.get(contextPath + '/products').then(function (response) {
-            $scope.productList = response.data;
+    $scope.loadProducts = function (indexPage = 1) {
+        $http({
+            url: contextPath + '/products',
+            method: 'GET',
+            params: {
+                title_part: $scope.filter ? $scope.filter.title_part : null,
+                min_cost: $scope.filter ? $scope.filter.min_cost : null,
+                max_cost: $scope.filter ? $scope.filter.max_cost : null,
+            }
+        }).then(function (response) {
+            $scope.productList = response.data.content;
         });
     };
 
